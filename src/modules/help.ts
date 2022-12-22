c3fc4241cfafbe773c0263b4b5728dd757852686
Aditya <me@xditya.me>
2022-12-22 07:37:37 +0530

    chore: update help message

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -6,10 +6,15 @@ composer.callbackQuery("help", async (ctx) => {
   await ctx.editMessageText(
     `<b>Help Menu:</b>
     
-Cloning the bot:
+<b>Cloning the bot:</b>
 1. Go to @BotFather and create a new bot.
 2. Forward the message with token from @BotFather to this bot.
 That's it!
+
+<b>Deleting the Clone:</b>
+1. Go to @BotFather.
+2. Send <code>/mybots</code>.
+3. Click on "API Token" and revoken token.
 
 <b>Using the bot:</b>
 Send or forward a message and use the buttons to get the message data.`,
